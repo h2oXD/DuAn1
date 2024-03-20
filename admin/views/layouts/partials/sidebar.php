8570897e4166b9797a92ede7d532799d4c9505ec
Vu Duc Tai <vutai0122@gmail.com>
2024-03-20 20:10:53 +0700

    latest

--- admin/views/layouts/partials/sidebar.php
+++ admin/views/layouts/partials/sidebar.php
@@ -46,11 +46,11 @@
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
                     aria-expanded="true" aria-controls="collapseTwo">
                     <i class="fas fa-fw fa-cog"></i>
-                    <span>Hàng hóa</span>
+                    <span>Loại hàng</span>
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Hàng hóa:</h6>
+                        <h6 class="collapse-header">Loại hàng:</h6>
                         <a class="collapse-item" href="<?= BASE_URL_ADMIN . '?act=users' ?>">Danh sách</a>
                         <a class="collapse-item" href="<?= BASE_URL_ADMIN . '?act=users-create' ?>">Thêm mới</a>
                     </div>
