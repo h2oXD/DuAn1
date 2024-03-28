0858be8ac2db596ebb9aff325b41ff1ddc4c86f4
h2oXD <fixbugandcry@gmail.com>
2024-03-29 02:16:25 +0700

    user

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -1,127 +1,139 @@
-<?php 
-    function userLoginRegister(){
-        $view = "users/login_register";
-        if(isset($_POST['login'])){
+<?php
+function userLoginRegister()
+{
+    $view = "users/login_register";
+    if (isset($_POST['login'])) {
+        $data = [
+            'email' => $_POST['login_email'] ?? null,
+            'password' => $_POST['login_password'] ?? null,
+            'remember' => $_POST['remember'] ?? null
+        ];
+        // validateUserUpdateDetail($data);
+        if (userLogin($data['email'], $data['password'])) {
+            $_SESSION['user'] = userLogin($data['email'], $data['password']);
+            header("Location: " . BASE_URL);
+        } else {
+            $_SESSION['faillogin'] = "Đăng nhập không thành công";
+        }
+
+    }
+    if (isset($_POST['register'])) {
+        $data = [
+            'email' => $_POST['register_email'] ?? null,
+            'password' => $_POST['register_password'] ?? null,
+            'confirm_password' => $_POST['confirm_password'] ?? null,
+        ];
+        // validateUserUpdateDetail($data);
+    }
+    require PATH_VIEW . 'layouts/master.php';
+}
+function userForgetPassword()
+{
+    $view = "users/forget_password";
+
+    require PATH_VIEW . 'layouts/master.php';
+}
+function accountDetail()
+{
+    $view = "users/account_detail";
+    if (isset($_POST['account_edit_form'])) {
+        $data = [
+            'name' => $_POST['name'] ?? $_SESSION['user']['name'],
+            'phone_number' => $_POST['phone_number'] ?? $_SESSION['user']['phone_number'],
+            'address' => $_POST['address'] ?? $_SESSION['user']['address'],
+        ];
+        foreach ($data as $key => $value) {
+            foreach ($_SESSION['user'] as $key2 => $value2) {
+                if ($key == $key2 && $value == $value2) {
+                    unset($data[$key]);
+                }
+            }
+        }
+        if(validateUserUpdateDetail($data)){
+            update('users', $_SESSION['user']['id'], $data);
+            foreach ($data as $key => $value) {
+                $_SESSION['user'][$key] = $value;
+            }
             
-            $data = [
-                'email' => $_POST['login_email'] ?? null,
-                'password' => $_POST['login_password'] ?? null,
-                'remember' => $_POST['remember'] ?? null
-            ];
-            // validateUserUpdateDetail($data);
-            if(userLogin($data['email'],$data['password'])){
-               $_SESSION['user'] = userLogin($data['email'],$data['password']);
-               header("Location: " . BASE_URL);
-            }else{
-                $_SESSION['faillogin'] = "Đăng nhập không thành công";
-            }
+            $_SESSION['successUpdateUser'] = "Cập nhật thông tin thành công";
 
-        }
-        if(isset($_POST['register'])){
-            $data = [
-                'email'             => $_POST['register_email']     ?? null,
-                'password'          => $_POST['register_password']  ?? null,
-                'confirm_password'  => $_POST['confirm_password']   ?? null,
-            ];
-            // validateUserUpdateDetail($data);
-        }
-        require PATH_VIEW.'layouts/master.php';
-    }
-    function userForgetPassword(){
-        $view = "users/forget_password";
-
-        require PATH_VIEW.'layouts/master.php';
-    }
-    function accountDetail(){
-        $view = "users/account_detail";
-        if(isset($_POST['account_edit_form'])){
-            $data = [
-                'name'                 => $_POST['name']                  ?? $_SESSION['user']['name'],
-                'phone_number'         => $_POST['phone_number']          ?? $_SESSION['user']['phone_number'],
-                'address'              => $_POST['address']               ?? $_SESSION['user']['address'],
-            ];
-            foreach($data as $key => $value){
-                foreach($_SESSION['user'] as $key2 => $value2){
-                    if($key == $key2 && $value == $value2){
-                        unset($data[$key]);
-                    }
-                }
-            }
-
-            validateUserUpdateDetail($data);
-             update('users', $_SESSION['user']['id'], $data);
-            
-           
-        }
-        if(isset($_POST['reset_pass'])){
-            $pass = $_SESSION['user']['password'];
-            $inputPass = $_POST['password'];
-            $newPass = $_POST['new_password'];
-            $confirmPass = $_POST['confirm_new_password'];
-            if(checkUserPassword($pass,$inputPass,$newPass,$confirmPass)){
-                $data['password'] = $newPass;
-                update('users', $_SESSION['user']['id'], $data);
-                $_SESSION['successResetPass'] = "Đổi mật khẩu thành công";
-
-            }
-
-            }
-        
-
-        require PATH_VIEW.'layouts/master.php';
-    }
-    function validateUserUpdateDetail($data){
-        if($data){
-            if(isset($data['name'])){
-                if(empty($data['name']) || strlen($data['name']) >= 50 || strlen($data['name']) <= 3){
-                    $_SESSION['errorName'] = "Tên phải có từ 3->50 ký tự";
-                }
-                
-            }
-
-            if(isset($data['phone_number'])){
-                if(empty($data['phone_number']) || strlen($data['phone_number']) < 10){
-                    $_SESSION['errorSDT'] = "Số điện thoại tối thiểu là 10 chữ số";
-                }
-               
-            }
-            if(isset($data['email'])){
-                if(empty($data['phone_number']) || filter_var($data['email'],FILTER_VALIDATE_EMAIL)){
-                    $_SESSION['errorEmail'] = "Email không đúng định dạng";
-                }
-               
-            }
         }
         
     }
-    function checkUserPassword($pass,$inputPass,$newPass = null ,$confirmPass = null){
-        // debug($inputPass);
-        $check = true;
-        if(empty($inputPass)){
-            $_SESSION['errorPass'] = "Bạn chưa nhập mật khẩu hiện tại";
-            $check = false;
-        }
-        if(!$newPass || strlen($newPass) < 8 || strlen($newPass) > 20){
-            $_SESSION['errorNewPass'] = "Mật khẩu mới phải từ 8->20 ký tự";
-            $check = false;
-        }else if ($pass == $newPass){
-            $_SESSION['errorNewPass'] = "Mật khẩu mới phải khác mật khẩu hiện tại";
-            $check = false;
-        }
-        if($newPass != $confirmPass){
-            $_SESSION['confirmPass'] = "Mật khẩu mới của bạn không trùng khớp";
-            $check = false;
-        }
-        if($check && $inputPass != $pass){
-            $_SESSION['errorPass'] = "Mật khẩu hiện tại không chính xác";
-            $check = false;
+    if (isset($_POST['reset_pass'])) {
+        $pass = $_SESSION['user']['password'];
+        $inputPass = $_POST['password'];
+        $newPass = $_POST['new_password'];
+        $confirmPass = $_POST['confirm_new_password'];
+        if (checkUserPassword($pass, $inputPass, $newPass, $confirmPass)) {
+            $data['password'] = $newPass;
+            update('users', $_SESSION['user']['id'], $data);
+            $_SESSION['successResetPass'] = "Đổi mật khẩu thành công";
+
         }
 
-        return $check;
     }
-    function userLogout(){
-        unset($_SESSION['user']);
-        $view = "home";
 
-        require PATH_VIEW.'layouts/master.php';
+
+    require PATH_VIEW . 'layouts/master.php';
+}
+function validateUserUpdateDetail($data)
+{
+    $check = true;
+    if ($data) {
+        
+        if (isset($data['name'])) {
+            if (empty($data['name']) || strlen($data['name']) >= 50 || strlen($data['name']) <= 3) {
+                $_SESSION['errorName'] = "Tên phải có từ 3->50 ký tự";
+                $check = false;
+            }
+
+        }
+
+        if (isset($data['phone_number'])) {
+            if (empty($data['phone_number']) || strlen($data['phone_number']) < 10) {
+                $_SESSION['errorSDT'] = "Số điện thoại tối thiểu là 10 chữ số";
+                $check = false;
+            }
+
+        }
+    }else{
+        $_SESSION['nothing'] = "Bạn chưa thay đổi gì cả!";
+        $check = false;
+        
     }
+    return $check;
+}
+function checkUserPassword($pass, $inputPass, $newPass = null, $confirmPass = null)
+{
+    // debug($inputPass);
+    $check = true;
+    if (empty($inputPass)) {
+        $_SESSION['errorPass'] = "Bạn chưa nhập mật khẩu hiện tại";
+        $check = false;
+    }
+    if (!$newPass || strlen($newPass) < 8 || strlen($newPass) > 20) {
+        $_SESSION['errorNewPass'] = "Mật khẩu mới phải từ 8->20 ký tự";
+        $check = false;
+    } else if ($pass == $newPass) {
+        $_SESSION['errorNewPass'] = "Mật khẩu mới phải khác mật khẩu hiện tại";
+        $check = false;
+    }
+    if ($newPass != $confirmPass) {
+        $_SESSION['confirmPass'] = "Mật khẩu mới của bạn không trùng khớp";
+        $check = false;
+    }
+    if ($check && $inputPass != $pass) {
+        $_SESSION['errorPass'] = "Mật khẩu hiện tại không chính xác";
+        $check = false;
+    }
+
+    return $check;
+}
+function userLogout()
+{
+    unset($_SESSION['user']);
+    $view = "home";
+
+    require PATH_VIEW . 'layouts/master.php';
+}
--- views/users/account_detail.php
+++ views/users/account_detail.php
@@ -1,98 +1,126 @@
 <main>
-    <div class="mb-4 pb-4"></div>
-    <section class="my-account container">
-      <h2 class="page-title">Thông tin cá nhân</h2>
-      <div class="row">
-        <div class="col-lg-3">
-          <ul class="account-nav">
-            <li><a href="?act=account_dashboard" class="menu-link menu-link_us-s">Dashboard</a></li>
-            <li><a href="?act=account_order" class="menu-link menu-link_us-s">Đơn mua</a></li>
-            <li><a href="?act=account_detail" class="menu-link menu-link_us-s menu-link_active">Thông tin cá nhân</a></li>
-            <li><a href="?act=logout" class="menu-link menu-link_us-s">Đăng xuất</a></li>
-          </ul>
-        </div>
-        <div class="col-lg-9">
-          <div class="page-content my-account__edit">
-            <div class="my-account__edit-form">
-              <form action="" method="post"  class="needs-validation" novalidate>
-                <div class="row">
-                  <div class="col-md-6">
-                    <div class="form-floating my-3">
-                      <input name="name" value="<?= $_SESSION['user']['name'] ?>" type="text" class="form-control" id="account_last_name" placeholder="Name" required>
-                      <label for="account_name">Họ và tên</label>
-                      <div style="color:rgba(231,74,59,.9)" class=".was-validated "><?= $_SESSION['errorName'] ?? null ?></div>
-                      <?php unset($_SESSION['errorName']); ?>
+  <div class="mb-4 pb-4"></div>
+  <section class="my-account container">
+    <h2 class="page-title">Thông tin cá nhân</h2>
+    <div class="row">
+      <div class="col-lg-3">
+        <ul class="account-nav">
+          <li><a href="?act=account_dashboard" class="menu-link menu-link_us-s">Dashboard</a></li>
+          <li><a href="?act=account_order" class="menu-link menu-link_us-s">Đơn mua</a></li>
+          <li><a href="?act=account_detail" class="menu-link menu-link_us-s menu-link_active">Thông tin cá nhân</a></li>
+          <li><a href="?act=logout" class="menu-link menu-link_us-s">Đăng xuất</a></li>
+        </ul>
+      </div>
+      <div class="col-lg-9">
+        <div class="page-content my-account__edit">
+          <div class="my-account__edit-form">
+            <form action="" method="post" class="needs-validation" novalidate>
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="form-floating my-3">
+                    <input name="name" value="<?= $_SESSION['user']['name'] ?>" type="text" class="form-control"
+                      id="account_last_name" placeholder="Name" required>
+                    <label for="account_name">Họ và tên</label>
+                    <div style="color:rgba(231,74,59,.9)" class=".was-validated ">
+                      <?= $_SESSION['errorName'] ?? null ?>
                     </div>
-                  </div>
-                  <div class="col-md-6">
-                    <div class="form-floating my-3">
-                      <input name="phone_number" value="<?= $_SESSION['user']['phone_number'] ?>" type="text" class="form-control" id="account_last_name" placeholder="Name" required>
-                      <label for="account_name">Số điện thoại</label>
-                      <div style="color:rgba(231,74,59,.9)" class=".was-validated"><?= $_SESSION['errorSDT'] ?? null ?></div>
-                      <?php unset($_SESSION['errorSDT']); ?>
-                    </div>
-                  </div>
-                  <div class="col-md-6">
-                    <div class="form-floating my-3">
-                      <input disabled name="email" value="<?= $_SESSION['user']['email'] ?>" type="email" class="form-control" id="account_email" placeholder="Email Address" required>
-                      <label for="account_email">Địa chỉ Email</label>
-                    </div>
-                  </div>
-                  <div class="col-md-6">
-                    <div class="form-floating my-3">
-                      <input name="address" value="<?= $_SESSION['user']['address'] ?>" type="text" class="form-control" id="account_address" placeholder="Address" required>
-                      <label for="account_address">Địa chỉ</label>
-                    </div>
-                  </div>
-                  <div class="col-md-12">
-                    <div class="my-3">
-                      <button name="account_edit_form" type="submit" class="btn btn-primary">Lưu thay đổi</button>
-                    </div>
-                  </div>
-              </form>  
-              <form action="" method="post"></form>
-                  <div class="col-md-12">
-                    <div class="my-3">
-                      <h5 class="text-uppercase mb-0">Thay đổi mật khẩu</h5>
-                    </div>
-                  </div>
-                  <div class="col-md-12">
-                    <div class="form-floating my-3">
-                      <input name="password" type="password" class="form-control" id="account_current_password" placeholder="Mật khẩu hiện tại" required>
-                      <label for="account_current_password">Mật khẩu hiện tại</label>
-                      <div style="color:rgba(231,74,59,.9)" class=".was-validated"><?= $_SESSION['errorPass'] ?? null ?></div>
-                      <?php unset($_SESSION['errorPass']); ?>
-                    </div>
-                  </div>
-                  <div class="col-md-12">
-                    <div class="form-floating my-3">
-                      <input name="new_password" type="password" class="form-control" id="account_new_password" placeholder="Mật khẩu mới" required>
-                      <label for="account_new_password">Mật khẩu mới</label>
-                      <div style="color:rgba(231,74,59,.9)" class=".was-validated"><?= $_SESSION['errorNewPass'] ?? null ?></div>
-                      <?php unset($_SESSION['errorNewPass']); ?>
-                    </div>
-                  </div>
-                  <div class="col-md-12">
-                    <div class="form-floating my-3">
-                      <input name="confirm_new_password" type="password" class="form-control" data-cf-pwd="#account_new_password" id="account_confirm_password" placeholder="Xác nhận mật khẩu mới" required>
-                      <label for="account_confirm_password">Xác nhận mật khẩu mới</label>
-                      <div class="invalid-feedback">Mật khẩu không đúng!</div>
-                      <div style="color:rgba(231,74,59,.9)" class=".was-validated"><?= $_SESSION['confirmPass'] ?? null ?></div>
-                      <?php unset($_SESSION['confirmPass']); ?>
-                    </div>
-                  </div>
-                  <div style="color:#22bb33" class=".was-validated"><?= $_SESSION['successResetPass'] ?? null ?></div>
-                      <?php unset($_SESSION['successResetPass']); ?>
-                  <div class="col-md-12">
-                    <div class="my-3">
-                      <button name="reset_pass" type="submit" class="btn btn-primary">Đổi mật khẩu</button>
-                    </div>
+                    <?php unset($_SESSION['errorName']); ?>
                   </div>
                 </div>
-              </form>
+                <div class="col-md-6">
+                  <div class="form-floating my-3">
+                    <input name="phone_number" value="<?= $_SESSION['user']['phone_number'] ?>" type="text"
+                      class="form-control" id="account_last_name" placeholder="Name" required>
+                    <label for="account_name">Số điện thoại</label>
+                    <div style="color:rgba(231,74,59,.9)" class=".was-validated">
+                      <?= $_SESSION['errorSDT'] ?? null ?>
+                    </div>
+                    <?php unset($_SESSION['errorSDT']); ?>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="form-floating my-3">
+                    <input disabled name="email" value="<?= $_SESSION['user']['email'] ?>" type="email"
+                      class="form-control" id="account_email" placeholder="Email Address" required>
+                    <label for="account_email">Địa chỉ Email</label>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="form-floating my-3">
+                    <input name="address" value="<?= $_SESSION['user']['address'] ?>" type="text" class="form-control"
+                      id="account_address" placeholder="Address" required>
+                    <label for="account_address">Địa chỉ</label>
+                  </div>
+                </div>
+                <div style="color:#22bb33" class=".was-validated ">
+                  <?= $_SESSION['successUpdateUser'] ?? null ?>
+                </div>
+                <?php unset($_SESSION['successUpdateUser']); ?>
+                <div style="color:#22bb33" class=".was-validated ">
+                  <?= $_SESSION['nothing'] ?? null ?>
+                </div>
+                <?php unset($_SESSION['nothing']); ?>
+                <div class="col-md-12">
+                  <div class="my-3">
+                    <button name="account_edit_form" type="submit" class="btn btn-primary">Lưu thay đổi</button>
+                  </div>
+                </div>
+            </form>
+            <form action="" method="post"></form>
+            <div class="col-md-12">
+              <div class="my-3">
+                <h5 class="text-uppercase mb-0">Thay đổi mật khẩu</h5>
+              </div>
+            </div>
+            <div class="col-md-12">
+              <div class="form-floating my-3">
+                <input name="password" type="password" class="form-control" id="account_current_password"
+                  placeholder="Mật khẩu hiện tại" required>
+                <label for="account_current_password">Mật khẩu hiện tại</label>
+                <div style="color:rgba(231,74,59,.9)" class=".was-validated">
+                  <?= $_SESSION['errorPass'] ?? null ?>
+                </div>
+                <?php unset($_SESSION['errorPass']); ?>
+              </div>
+            </div>
+            <div class="col-md-12">
+              <div class="form-floating my-3">
+                <input name="new_password" type="password" class="form-control" id="account_new_password"
+                  placeholder="Mật khẩu mới" required>
+                <label for="account_new_password">Mật khẩu mới</label>
+                <div style="color:rgba(231,74,59,.9)" class=".was-validated">
+                  <?= $_SESSION['errorNewPass'] ?? null ?>
+                </div>
+                <?php unset($_SESSION['errorNewPass']); ?>
+              </div>
+            </div>
+            <div class="col-md-12">
+              <div class="form-floating my-3">
+                <input name="confirm_new_password" type="password" class="form-control"
+                  data-cf-pwd="#account_new_password" id="account_confirm_password" placeholder="Xác nhận mật khẩu mới"
+                  required>
+                <label for="account_confirm_password">Xác nhận mật khẩu mới</label>
+                <div class="invalid-feedback">Mật khẩu không đúng!</div>
+                <div style="color:rgba(231,74,59,.9)" class=".was-validated">
+                  <?= $_SESSION['confirmPass'] ?? null ?>
+                </div>
+                <?php unset($_SESSION['confirmPass']); ?>
+              </div>
+            </div>
+            <div style="color:#22bb33" class=".was-validated">
+              <?= $_SESSION['successResetPass'] ?? null ?>
+            </div>
+            <?php unset($_SESSION['successResetPass']); ?>
+            <div class="col-md-12">
+              <div class="my-3">
+                <button name="reset_pass" type="submit" class="btn btn-primary">Đổi mật khẩu</button>
+              </div>
             </div>
           </div>
+          </form>
         </div>
       </div>
-    </section>
-  </main>
+    </div>
+    </div>
+  </section>
+</main>
