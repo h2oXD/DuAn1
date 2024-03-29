e2b4494e6120edd6576016bd817d5738ec946ee4
h2oXD <fixbugandcry@gmail.com>
2024-03-29 13:15:03 +0700

    finail user real

--- admin/models/User.php
+++ admin/models/User.php
@@ -1,25 +1,5 @@
 <?php 
-if (!function_exists('checkUniqueEmail')) {
-    function checkUniqueEmail($tableName, $email)
-    {
-        try {
-            $sql = "SELECT * FROM $tableName WHERE email = :email LIMIT 1";
 
-            $stmt = $GLOBALS['conn']->prepare($sql);
-
-            $stmt->bindParam(":email", $email);
-
-            $stmt->execute();
-
-            $data = $stmt->fetch();
-
-            return empty($data) ? true : false;
-        } catch (\Exception $e) {
-            debug($e);
-        }
-
-    }
-}
 if (!function_exists('checkUniqueEmailForUpdate')) {
     function checkUniqueEmailForUpdate($tableName,$id, $email)
     {
